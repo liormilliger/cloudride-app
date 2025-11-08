20dc7b8c6ae1597c4f7ac7e3bdb27f7f5d7fd452
Lior Milliger <liormilliger@gmail.com>
2025-11-08 21:05:46 +0200

    IAM policy for ALB moved to eks module && patch_argocd_finalizers.sh script added

--- terraform/eks/eks.tf
+++ terraform/eks/eks.tf
@@ -68,3 +68,46 @@ resource "aws_iam_openid_connect_provider" "eks_oidc_provider" {
 data "tls_certificate" "eks_cluster_cert" {
   url = aws_eks_cluster.eks-cluster.identity[0].oidc[0].issuer
 }
+
+resource "aws_iam_policy" "alb_controller_policy" {
+  name        = "${var.cluster_name}-alb-controller-policy"
+  description = "Permissions for AWS Load Balancer Controller"
+  policy      = file("${path.module}/policies/iam-policy.json")
+}
+
+resource "aws_iam_role_policy_attachment" "alb_controller_policy_attach" {
+  role       = aws_iam_role.alb_controller_iam_role.name
+  policy_arn = aws_iam_policy.alb_controller_policy.arn
+}
+
+# -----------------------------------------------------------------------------
+# ALB Controller IAM Role (For IRSA)
+# -----------------------------------------------------------------------------
+resource "aws_iam_role" "alb_controller_iam_role" {
+  # Naming convention matches the cluster name for easy identification
+  name = "${var.cluster_name}-alb-controller-role" 
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Federated": "${aws_iam_openid_connect_provider.eks_oidc_provider.arn}"
+      },
+      "Action": "sts:AssumeRoleWithWebIdentity",
+      "Condition": {
+        "StringEquals": {
+          "${aws_iam_openid_connect_provider.eks_oidc_provider.url}:sub": "system:serviceaccount:kube-system:aws-load-balancer-controller"
+        }
+      }
+    }
+  ]
+}
+POLICY
+
+  tags = {
+    provisioned_by = "Terraform"
+  }
+}
